58a8dfb0db3fbb97fa08354f721ac6d42b002fe3
Nicolas Reboux <nr919n@209.data-dev.user.nym2.adnexus.net>
2025-01-03 16:43:12 +0000

    Update Streamlit app with modular structure

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from value-guessr!")
+from src.valueguessr.utils import greet_user
+import streamlit as st
 
+def main():
+    st.title("Value Guessr")
+    name = st.text_input("Enter your name:")
+    if name:
+        greeting = greet_user(name)
+        st.write(greeting)
 
 if __name__ == "__main__":
     main()
